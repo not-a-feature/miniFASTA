86622d445d56df367e7a6b294004a205156bbfa4
Jules Kreuer <contact@juleskreuer.eu>
2021-12-20 12:17:27 +0100

    init

---
+++ src/miniFasta/miniFasta.py
@@ -0,0 +1,243 @@
+"""
+@author: Jules Kreuer
+A simple fasta object, reader, writer and translator
+"""
+
+from os import path
+
+# Usual translation dictionary according to
+# https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG1
+translation_dict = {"TTT": "F", "TTC": "F",
+                    "TTA": "L", "TTG": "L",
+                    "TCT": "S", "TCC": "S", "TCA": "S", "TCG": "S",
+                    "TAT": "Y", "TAC": "Y",
+                    "TAA": "*", "TAG": "*", "TGA": "*",
+                    "TGT": "C", "TGC": "C",
+                    "TGG": "W",
+                    "CTT": "L", "CTC": "L", "CTA": "L", "CTG": "L",
+                    "CCT": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+                    "CAT": "H", "CAC": "H",
+                    "CAA": "Q", "CAG": "Q",
+                    "CGT": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+                    "ATT": "I", "ATC": "I", "ATA": "I",
+                    "ATG": "M",
+                    "ACT": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+                    "AAT": "N", "AAC": "N",
+                    "AAA": "K", "AAG": "K",
+                    "AGT": "S", "AGC": "S",
+                    "AGA": "R", "AGG": "R",
+                    "GTT": "V", "GTC": "V", "GTA": "V", "GTG": "V",
+                    "GCT": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+                    "GAT": "D", "GAC": "D",
+                    "GAA": "E", "GAG": "E",
+                    "GGT": "G", "GGC": "G", "GGA": "G", "GGG": "G"}
+
+# Usual reverse complement
+complement_dict = {"A": "T",
+                   "G": "C",
+                   "C": "G",
+                   "T": "A",
+                   "U": "A",
+                   "R": "Y",
+                   "Y": "R",
+                   "S": "S",
+                   "W": "W",
+                   "K": "M",
+                   "M": "K",
+                   "B": "V",
+                   "V": "B",
+                   "D": "H",
+                   "H": "D"}
+
+
+class fasta_object():
+    def __init__(self, head: str, body: str):
+        """
+        Object to keep a valid fasta object
+        """
+        if head.startswith(">"):
+            self.head = head
+        else:
+            self.head = f">{head}"
+
+        self.body = body
+
+    def __str__(self):
+        """
+        Magic method to allow fasta_object printing.
+        """
+        out = f'{self.head}\n'
+
+        # Print only 70 chars per line
+        for i in range(0, len(self.body), 70):
+            out += f'{self.body[i:i+70]}\n'
+
+        # Remove tailing newline
+        return out[:-1]
+
+    def __repr__(self):
+        """
+        Magic method to allow printing of fasta_object representation.
+        """
+        return f'fasta_object("{self.head}", "{self.body}")'
+
+    def __eq__(self, o):
+        """
+        Magic method to allow equality check on fasta_objects.
+        Does not check for header equality.
+        """
+        return self.body == o.body
+
+    def __len__(self):
+        """
+        Magic method to allow len() on fasta_objects.
+        Does not check for header-length equality.
+        """
+        return len(self.body)
+
+    def toAmino(self, d=translation_dict):
+        """
+        Translates the dna sequence of a fasta_object to amino-acids.
+        Reading frame starts at position 0, tailing bases will be ignored.
+        Attention: Will replace triplet with ~ if not found.
+        Input:
+            d: dict, dictionary of translation.
+        """
+        self.body = translate_seq(self.body, d)
+
+    def toRevComp(self,  d=complement_dict):
+        """
+        Reverses complement of sequence.
+        If no complement was found, the nucleotides remains unchanged.
+        Input:
+            d: dict, dictionary of complement.
+        """
+        self.body = reverse_comp(self.body, d)
+
+
+def read_fasta(fileName: str, upper: bool = True):
+    """
+    Reads a fasta-style file and returns a list of fasta_objects.
+    """
+
+    if not path.isfile(fileName):
+        raise FileNotFoundError("Fasta File not found!")
+
+    fasta_objects = []
+    with open(fileName, 'r') as f:
+        head = ""
+        body = ""
+        newObject = True
+        for line in f:
+            # go through each line
+            # First Header
+            if newObject and line.startswith(">"):
+                head = line.strip()
+                body = ""
+                newObject = False
+            # N-th Header
+            elif line.startswith(">"):
+                fasta_objects.append(fasta_object(head, body))
+                head = line.strip()
+                body = ""
+            # Sequence
+            else:
+                addBody = line.strip()
+                if upper:
+                    addBody = addBody.upper()
+
+                body += addBody
+        # append last element
+        fasta_objects.append(fasta_object(head, body))
+    return fasta_objects
+
+
+def write_fasta(fasta_pairs, fileName: str, mode="w"):
+    """
+    Writes a list of fasta_objects or a single one to a file.
+    Takes fasta_objects as input.
+    """
+
+    if not isinstance(fasta_pairs, list):
+        fasta_pairs = [fasta_pairs]
+
+    with open(fileName, mode) as f:
+        for fo in fasta_pairs:
+            f.write(f"{fo.head}\n")
+            body_len = len(fo.body)
+            # Write only 70 chars per line
+            for i in range(0, body_len, 70):
+                f.write(f"{fo.body[i:i+70]}\n")
+    return True
+
+
+def print_fasta(fasta):
+    """
+    Prints a single or a list of fasta_objects.
+    """
+
+    if not isinstance(fasta, list):
+        fasta = [fasta]
+
+    for fo in fasta:
+        print(fo.head)
+        body_len = len(fo.body)
+        # Print only 70 chars per line
+        for i in range(0, body_len, 70):
+            print(fo.body[i:i+70])
+    return None
+
+
+def __maybeFind(key, d, alt):
+    """
+    Tries to find key in dict but has a fallback.
+    Input:
+        key: hashable, key to find.
+        d: dict, dictionary to search.
+        alt: alternative if key not found.
+    """
+    try:
+        return d[key]
+    except KeyError:
+        return alt
+
+
+def translate_seq(seq: str, d=translation_dict):
+    """
+    Translates a DNA sequence to a AA sequence.
+    Reading frame starts at position 0, tailing bases will be ignored.
+    Attention: Will replace triplet with ~ if not found.
+
+    To translate a fasta_object use object.toAmino()
+
+    Input:
+        seq: String, sequence to translate.
+        d: dict, dictionary of translation.
+    Returns:
+        translated: String, translated sequence.
+    """
+
+    translated = ""
+    for i in range(0, len(seq), 3):
+        codon = seq[i:i+3]
+        if not len(codon) == 3:
+            break
+        translated += __maybeFind(codon, d, "~")
+    return translated
+
+
+def reverse_comp(seq: str, d=complement_dict):
+    """
+    Reverses complement of sequence.
+    If no complement was found, the nucleotides remains unchanged.
+    Input:
+        seq: String, sequence to compute the reverse complement.
+        d: dict, dictionary of complement.
+    Returns:
+        rev: String, translated sequence
+    """
+    if seq == "":
+        return ""
+    rev = reversed(seq)
+    rev = "".join(map(lambda b: __maybeFind(b, complement_dict, b), rev))  # type:ignore
+    return rev
---
+++ tests/test_fasta.py
@@ -0,0 +1,84 @@
+import pytest
+from miniFasta import miniFasta as fasta
+from os import path, remove
+
+
+def test_read_fasta():
+    file_path = path.join(path.dirname(__file__), "test_data/test0.fasta")
+
+    assert fasta.read_fasta(file_path) == [
+        fasta.fasta_object(">Atlantic dolphin", "CGGCCTTCTATCTTCTTC"),
+        fasta.fasta_object(">Pacific dolphin", "CTTTCTATCTCTTTCCTCT")]
+
+
+def test_write_read_fasta():
+
+    file_path = path.join(path.dirname(__file__), "write_fasta_test.fasta")
+
+    fo = [fasta.fasta_object(">Atlantic dolphin", "CGGCCTT*CTAAAAATTZZZ*ZZZZASASD*TCTTCTTC"),
+          fasta.fasta_object(">Pacific dolphin", "CTTTCTATCTCSATTTCCTCT")]
+
+    fasta.write_fasta(fo, file_path)
+
+    fo_read = fasta.read_fasta(file_path)
+    remove(file_path)
+    assert fo == fo_read
+
+
+def test_read_strange_header():
+    file_path = path.join(path.dirname(__file__), "test_data/test1.fasta")
+    fos_heads = [fo.head for fo in fasta.read_fasta(file_path)]
+    assert fos_heads == [""">FirstTestFASTA !"§$%_-&/()=?ß'#'"",.\\ :""", ">Second Reguar", ">"]
+
+
+def test_read_long_body():
+    file_path = path.join(path.dirname(__file__), "test_data/test2.fasta")
+    b = "".join(["CCAGTCTGGTCTCTCTCTAGATCAATTTTAACGGGCAAATTGTTGCTATTGCTCCAAATTCAATGGGACGCGCCTCT",
+                 "TAGTCAGAAAGTACACGCAAACTACATGCTAGTAATGCCTCTGGGCAGGTATGTGGAGGACCGGCATTAGCGTGGCGC",
+                 "GTTCGCAGGGGGATTGGCGAACCGAAGGTGACCTTAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGTCTGAACC",
+                 "ACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGTCTGAAC",
+                 "CACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGTCTGAA",
+                 "CCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGTCTGA",
+                 "ACCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGTCTG",
+                 "AACCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGTCT",
+                 "GAACCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGTC",
+                 "TGAACCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAGT",
+                 "CTGAACCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACAG",
+                 "TCTGAACCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAATAGGGATATTGTGTTGGGAGCGCGGTCCCCCTACA",
+                 "GTCTGAACCACCCCTTCAAGAGTCGCTAGGAAGCTCTTGGTCAA"])
+    assert fasta.read_fasta(file_path)[0].body == b
+
+
+def test_translate_seq():
+    assert fasta.translate_seq("CGGCCTTCTATCTTCTTC") == "RPSIFF"
+    assert fasta.translate_seq("HELLO") == "~"
+
+
+def test_translate_toAmino():
+    fo = fasta.fasta_object("test", "CGGCCTTCTATCTTCTTC")
+    fo.toAmino()
+    assert fo.body == "RPSIFF"
+
+
+def test_reverse_comp_seq():
+    assert fasta.reverse_comp("CGGCCTTCTATCTTCTTC") == "GAAGAAGATAGAAGGCCG"
+
+
+def test_reverse_comp_toRevComp():
+    fo = fasta.fasta_object("test", "CGGCCTTCTATCTTCTTC")
+    fo.toRevComp()
+    assert fo.body == "GAAGAAGATAGAAGGCCG"
+
+
+def test_print_fasta(capsys):
+    file_path = path.join(path.dirname(__file__), "test_data/test0.fasta")
+
+    assert fasta.print_fasta(fasta.read_fasta(file_path)) is None
+
+    # check if it prints the sequences correctly
+    c = capsys.readouterr()
+    assert c.out == ">Atlantic dolphin\nCGGCCTTCTATCTTCTTC\n>Pacific dolphin\nCTTTCTATCTCTTTCCTCT\n"
+
+
+def test_len_fasta():
+    assert len(fasta.fasta_object("test", "abc")) == 3
