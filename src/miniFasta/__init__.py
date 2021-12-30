9372d4959b14deecd9990a1d507adc535580e2b8
Jules Kreuer <contact@juleskreuer.eu>
2021-12-30 20:25:45 +0100

    rename file to private

--- src/miniFasta/__init__.py
+++ src/miniFasta/__init__.py
@@ -1,4 +1,4 @@
-from .miniFasta import fasta_object, read, write, print_fasta, translate_seq, reverse_comp
+from ._miniFasta import fasta_object, read, write, print_fasta, translate_seq, reverse_comp
 
 __all__ = [
     "fasta_object",
